83a26afacd2bec4d9f6b5fb3b84f9d54f061e675
Gabriel Fernando Missaka Mendes <gabrielfmm@al.insper.edu.br>
2023-04-20 01:52:43 -0300

    Código atualizada (tela de instruções adicionada).

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
-from perguntas_e_respostas import *
-from funcoes import *
 # Dados gerais do jogo.
 WIDTH = 640 # Largura da tela
 HEIGHT = 480 # Altura da tela
@@ -7,6 +5,5 @@ FPS = 60 # Frames por segundo
 
 # Variáveis
 tela_de_inicio = True
+tela_de_instrucoes = False
 inicio_jogo = False
-
-questao_sorteada = sorteia_questao(perguntas,'medio')
--- jogo.py
+++ jogo.py
@@ -15,7 +15,7 @@ mixer.init()
 # ----- Gera tela principal
 window = display.set_mode((WIDTH, HEIGHT))
 clock = time.Clock()
-display.set_caption('Fast Game')
+display.set_caption('FastGame')
 
 
 # Símbolo do Pygame
@@ -28,12 +28,53 @@ while rodando:
     window.fill ((230, 226, 216))
     if tela_de_inicio:
         window.blit(logo, (WIDTH/2 - logo.get_width()/2, HEIGHT/2 - logo.get_height()/2 - 80))
-        fonte = fonte_jogo.render('Pressione qualquer tecla para começar', True, (0, 0, 0))
-        window.blit(fonte, (WIDTH/2 - fonte.get_width()/2, HEIGHT/2 - fonte.get_height()/2 + 80))
+        start = fonte_jogo.render('Pressione qualquer tecla para começar', True, (0, 0, 0))
+        instrucoes = fonte_jogo.render('Pressione i para ler as instruções do jogo', True, (0, 0, 0))
+        window.blit(start, (WIDTH/2 - start.get_width()/2, HEIGHT/2 - start.get_height()/2 + 80))
+        window.blit(instrucoes, (WIDTH/2 - instrucoes.get_width()/2, HEIGHT/2 - instrucoes.get_height()/2 + 120))
 
     else:
         
-        if inicio_jogo:
+        if tela_de_instrucoes:
+            instrucoes = fonte_jogo.render('Instruções', True, (0, 0, 0))
+            instrucoes_detalhe = fonte_jogo.render('----------', True, (0, 0, 0))
+            regras = fonte_jogo.render('Assim que o jogo for iniciado surgirá uma tela', True, (0, 0, 0))
+            regras1 = fonte_jogo.render('com uma pergunta e quatro opções de resposta.', True, (0, 0, 0))
+            regras2 = fonte_jogo.render('O jogador deverá escolher a resposta correta', True, (0, 0, 0))
+            regras3 = fonte_jogo.render('antes do tempo acabar, caso contrário o jogador', True, (0, 0, 0))
+            regras4 = fonte_jogo.render('perderá e o jogo será finalizado.', True, (0, 0, 0))
+            regras5 = fonte_jogo.render('Caso o jogador responda corretamente, ele poderá', True, (0, 0, 0))
+            regras6 = fonte_jogo.render('continuar com as perguntas e a pontuação do jogo', True, (0, 0, 0))
+            regras7 = fonte_jogo.render('será aumentada.', True, (0, 0, 0))
+            regras8 = fonte_jogo.render('Ao progredir no jogo, o nível das questões irá', True, (0, 0, 0))
+            regras9 = fonte_jogo.render('aumentar, fazendo com que as perguntas fiquem', True, (0, 0, 0))
+            regras10 = fonte_jogo.render('mais difíceis e o tempo de resposta diminua.', True, (0, 0, 0))
+            regras11 = fonte_jogo.render('Para ganhar o jogo, o jogador precisa responder', True, (0, 0, 0))
+            regras12 = fonte_jogo.render('todas as perguntas corretamente sem errar nenhuma.', True, (0, 0, 0))
+            inicio = fonte_jogo.render('Pressione i para voltar a tela inicial', True, (0, 0, 0))
+
+            window.blit(instrucoes, (WIDTH/2 - instrucoes.get_width()/2 - 220, HEIGHT/2 - instrucoes.get_height()/2 - 200))
+            window.blit(instrucoes_detalhe, (WIDTH/2 - instrucoes_detalhe.get_width()/2 - 220, HEIGHT/2 - instrucoes_detalhe.get_height()/2 - 185))
+            window.blit(regras, (WIDTH/2 - instrucoes.get_width()/2 - 220, HEIGHT/2 - instrucoes.get_height()/2 - 150))
+            window.blit(regras1, (WIDTH/2 - instrucoes.get_width()/2 - 220, HEIGHT/2 - instrucoes.get_height()/2 - 130))
+            window.blit(regras2, (WIDTH/2 - instrucoes.get_width()/2 - 220, HEIGHT/2 - instrucoes.get_height()/2 - 100))
+            window.blit(regras3, (WIDTH/2 - instrucoes.get_width()/2 - 220, HEIGHT/2 - instrucoes.get_height()/2 - 80))
+            window.blit(regras4, (WIDTH/2 - instrucoes.get_width()/2 - 220, HEIGHT/2 - instrucoes.get_height()/2 - 60))
+            window.blit(regras5, (WIDTH/2 - instrucoes.get_width()/2 - 220, HEIGHT/2 - instrucoes.get_height()/2 - 30))
+            window.blit(regras6, (WIDTH/2 - instrucoes.get_width()/2 - 220, HEIGHT/2 - instrucoes.get_height()/2 - 10))
+            window.blit(regras7, (WIDTH/2 - instrucoes.get_width()/2 - 220, HEIGHT/2 - instrucoes.get_height()/2 + 10))
+            window.blit(regras8, (WIDTH/2 - instrucoes.get_width()/2 - 220, HEIGHT/2 - instrucoes.get_height()/2 + 40))
+            window.blit(regras9, (WIDTH/2 - instrucoes.get_width()/2 - 220, HEIGHT/2 - instrucoes.get_height()/2 + 60))
+            window.blit(regras10, (WIDTH/2 - instrucoes.get_width()/2 - 220, HEIGHT/2 - instrucoes.get_height()/2 + 80))
+            window.blit(regras11, (WIDTH/2 - instrucoes.get_width()/2 - 220, HEIGHT/2 - instrucoes.get_height()/2 + 110))
+            window.blit(regras12, (WIDTH/2 - instrucoes.get_width()/2 - 220, HEIGHT/2 - instrucoes.get_height()/2 + 130))
+            window.blit(inicio, (WIDTH/2 - inicio.get_width()/2, HEIGHT/2 - inicio.get_height()/2 + 180))
+
+    
+           
+
+
+        elif inicio_jogo:
             #Pergunta do jogo
             fonte = fonte_jogo.render(sorteia_questao(perguntas,'facil')['titulo'], True, (238, 138, 111))
             window.blit (fonte, (WIDTH/3 - fonte.get_width()/2, HEIGHT/10 - fonte.get_height()/2))
@@ -54,9 +95,17 @@ while rodando:
             rodando = False
         elif evento.type == KEYDOWN:
             if tela_de_inicio:
-                inicio_jogo = True
-                tela_de_inicio = False
-
+                if evento.key == K_i:
+                    tela_de_inicio = False
+                    tela_de_instrucoes = True
+                else:
+                    tela_de_inicio = False
+                    inicio_jogo = True
+            else:
+                if evento.key == K_i:
+                    tela_de_inicio = True
+                    tela_de_instrucoes = False
+            
     clock.tick(FPS)
     display.update()
 
